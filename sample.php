6a70bbd6b4936530fbf8c7c7f15ca7a24443aa3c
hikarukadota-hash <kappynopasokon@yahoo.co.jp>
2021-06-22 13:34:53 +0900

    サンプルページ追加

---
+++ sample.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+
+<body>
+    <h1>ホリマニア</h1>
+    <img src="img/20200914090327-0.jpeg" width="120px" height="120px" alt=""><br>
+    ↑↓<br>
+    <img src="img/20200819012115-0.jpeg" width="120px" height="120px" alt="">
+    <form action="my_page.php" method="POST">
+        <fieldset>
+            <div>
+                <button>拒否</button>
+            </div>
+        </fieldset>
+    </form>
+    <form action="trad_success.php" method="POST">
+        <fieldset>
+            <div>
+                <button>承諾</button>
+            </div>
+        </fieldset>
+    </form>
+</body>
+
+</html>
